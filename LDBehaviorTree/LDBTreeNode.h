09ffc8ed0706ae6ae00f437021ef2338357b85e8
DrakeLu <540659251@qq.com>
2015-06-05 11:12:25 +0800

    add LDBTreeNode.h

---
+++ LDBehaviorTree/LDBTreeNode.h
@@ -0,0 +1,13 @@
+//
+//  LDBTreeNode.h
+//  LDBehaviorTree
+//
+//  Created by Ludi on 6/5/15.
+//  Copyright (c) 2015 Ludi. All rights reserved.
+//
+
+#ifndef LDBehaviorTree_LDBTreeNode_h
+#define LDBehaviorTree_LDBTreeNode_h
+
+
+#endif
