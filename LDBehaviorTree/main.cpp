d4ed14e768b3dc28f4cebe2df5f05b9a170171d2
DrakeLu <540659251@qq.com>
2015-06-05 11:09:58 +0800

    first commit

---
+++ LDBehaviorTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LDBehaviorTree
+//
+//  Created by Ludi on 6/5/15.
+//  Copyright (c) 2015 Ludi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
